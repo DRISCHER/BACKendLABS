26b7133e3002606c710204946c3d81a4307adbd8
DRISCHER <andriistupak88@gmail.com>
2024-01-19 16:12:55 +0200

    set up

---
+++ lab3/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import uuid
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py', silent=True)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+import lab3.views
+import lab3.models
+
